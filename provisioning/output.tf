173ba63b72a533752638d129f3dd827c9d4847ba
Khasan Abdurakhmanov <64947215+hasan2001jk@users.noreply.github.com>
2024-03-05 18:59:48 +0300

    Update output.tf

--- provisioning/output.tf
+++ provisioning/output.tf
@@ -16,3 +16,12 @@ output "internal_ip_address_k8sworker" {
 output "external_ip_address_k8sworker" {
   value = yandex_compute_instance.k8sworker.network_interface.0.nat_ip_address
 }
+
+# sonarqube
+output "internal_ip_address_sonarqube" {
+  value = yandex_compute_instance.sonarqube.network_interface.0.ip_address
+}
+
+output "external_ip_address_sonarqube" {
+  value = yandex_compute_instance.sonarqube.network_interface.0.nat_ip_address
+}
